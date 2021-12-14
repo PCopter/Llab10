cdfb05269a6e6d32866d1a33d48359fb90d27814
Copter <copter.phanuwat@gamil.com>
2021-12-14 17:04:46 +0700

    lab10 OK

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,14 +2,20 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan,payment,interestrateperyear;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interestrateperyear;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
+	
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -20,14 +26,26 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+
+double prevbalance=loan,interest,total,newbalance;
+	for(int count = 1;newbalance>0;count++){
+		interest = prevbalance*(interestrateperyear/100);
+		total = prevbalance+interest;
+		
+		if(total<payment) payment=total;
+
+		newbalance=total-payment;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << count; 
+		cout << setw(13) << left << prevbalance;
+		cout << setw(13) << left << interest;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << payment;
+		cout << setw(13) << left << newbalance;
+		cout << "\n";	
+		prevbalance=newbalance;
 	
+	}	
 	return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,23 +2,36 @@
 using namespace std;
 
 //Write the function printO() here
+int printO(int M,int N)
+{
+	if(M==0 or N<0)
+	{
+	cout << "Invalid input";
+	}else{
+		for(int x=0 ; x<M ; x++)
+		{
+			for(int y=0 ; y<N ; y++)
+			{
+			cout << "O";
+			}
+		cout << "\n";	
+		}
+	
+	}
+	return 0;
+}
+
 
 int main(){
-	
 	printO(2,2);
 	cout << "\n";
-	
 	printO(3,5);
 	cout << "\n";	
-	
 	printO(5,3);
 	cout << "\n";	
-	
 	printO(0,3);
 	cout << "\n";	
-	
 	printO(7,-1);
 	cout << "\n";	
-	
 	return 0;
 }
